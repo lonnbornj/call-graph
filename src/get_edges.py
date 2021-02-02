d9b7fa51b2a767bd789a28367acbba9be3b8c936
Jack Lonnborn <jack.lonnborn@gmail.com>
2021-02-02 20:22:13 +1100

    docstrings, formatting and renaming

--- src/get_edges.py
+++ src/get_edges.py
@@ -5,6 +5,7 @@ import glob
 
 
 def process_dir(root_dir, force=False):
+    """Process a directory (`root_dir`/code) of .m functions, saving data about their relationships"""
     src_files = glob.glob(os.path.join(root_dir, "code", "*.m"))
     fn_names = [os.path.basename(f).split(".")[0] for f in src_files]
     out_dir = _out_dir(root_dir)
@@ -16,26 +17,28 @@ def process_dir(root_dir, force=False):
         _write_edges_data(node, (src_files, fn_names), force, out_dir)
 
 
-def _write_edges_data(child, fns, force, out_dir):
-    edges_in_outfile = os.path.join(out_dir, child + "_in.txt")
-    edges_out_outfile = os.path.join(out_dir, child + "_out.txt")
+def _write_edges_data(cur_node, fns, force, out_dir):
+    """Write edges into and out of a node to text files"""
+    edges_in_outfile = os.path.join(out_dir, cur_node + "_in.txt")
+    edges_out_outfile = os.path.join(out_dir, cur_node + "_out.txt")
     if not force and os.path.isfile(edges_in_outfile):
         return
     Path(edges_out_outfile).touch(
         exist_ok=True
     )  # create empty edges-out file in case child is a leaf
-    print("Finding edges into node {}".format(child))
-    node_edges_in = _find_edges_in(child, fns)
+    print("Finding edges into node {}".format(cur_node))
+    node_edges_in = _find_edges_in(cur_node, fns)
     with open(edges_in_outfile, "w") as f:
         f.writelines([fn + "\n" for fn in node_edges_in])
     for other_edges_out_outfile in [
         os.path.join(out_dir, fn + "_out.txt") for fn in node_edges_in
     ]:
         with open(other_edges_out_outfile, "a") as f:
-            f.write(child + "\n")
+            f.write(cur_node + "\n")
 
 
 def _find_edges_in(child, fns):
+    """Find the edges into a node"""
     child_edges_in = list()
     for filen, fn in zip(*fns):
         if _file_mentions_child(filen, child):
@@ -44,6 +47,7 @@ def _find_edges_in(child, fns):
 
 
 def _file_mentions_child(filen, child):
+    """Returns True if a .m function mentions (calls) a specified child function, else returns False"""
     with open(filen, "r") as f:
         for line in f:
             if re.search(child, line):
@@ -52,6 +56,7 @@ def _file_mentions_child(filen, child):
 
 
 def read_edges_file(root_dir, node, graph_type):
+    """Read in data of edges into/out of a node"""
     fname_suffix = "_in" if graph_type == "leaf" else "_out"
     with open(os.path.join(_out_dir(root_dir), node + fname_suffix + ".txt")) as f:
         edge_fns = f.readlines()
@@ -60,10 +65,12 @@ def read_edges_file(root_dir, node, graph_type):
 
 
 def _out_dir(root_dir):
+    """Returns the output directory path"""
     return os.path.join(root_dir, "data")
 
 
 def _reset_output_files(directory):
+    """Remove previously computed edge data from the output directory"""
     for f in glob.glob(directory + "*.txt"):
         if os.path.isfile(f):
             os.remove(f)
--- src/make_call_graph.py
+++ src/make_call_graph.py
@@ -8,9 +8,10 @@ import get_edges
 import plotting
 
 
-def add_all_nodes(G, child, graph_type, root_dir, processed=[]):
-    G = add_relatives_to_graph(G, child, graph_type, root_dir)
-    processed.append(child)
+def add_all_nodes(G, cur_node, graph_type, root_dir, processed=[]):
+    """Populate the graph, by recursively find nodes connected by directed path"""
+    G = add_relatives_to_graph(G, cur_node, graph_type, root_dir)
+    processed.append(cur_node)
     cur_nodes = copy.deepcopy(G.nodes)
     for node in cur_nodes:
         if node not in processed:
@@ -18,20 +19,22 @@ def add_all_nodes(G, child, graph_type, root_dir, processed=[]):
     return G
 
 
-def add_relatives_to_graph(G, child, graph_type, root_dir):
-    relatives = get_edges.read_edges_file(root_dir, child, graph_type)
+def add_relatives_to_graph(G, cur_node, graph_type, root_dir):
+    """Add immediate ancestors/progeny to the graph; i.e. parents and children of the given node"""
+    relatives = get_edges.read_edges_file(root_dir, cur_node, graph_type)
     for relative in relatives:
         if graph_type == "leaf":
-            G.add_edges_from([(relative, child)])
+            G.add_edges_from([(relative, cur_node)])
         elif graph_type == "root":
-            G.add_edges_from([(child, relative)])
+            G.add_edges_from([(cur_node, relative)])
     G.remove_edges_from(nx.selfloop_edges(G))
     return G
 
 
 def calc_node_positions(G, graph_type, max_depth):
+    """Calculate the node positions"""
     layers = calc_graph_hierarchy(copy.deepcopy(G), graph_type)
-    num_layers = max([l for l in layers.values()])
+    num_layers = max(1, max([l for l in layers.values()]))
     cur_counts = defaultdict(lambda: 0)
     pos = nx.spring_layout(G)
     for node, layer in layers.items():
@@ -42,6 +45,7 @@ def calc_node_positions(G, graph_type, max_depth):
 
 
 def calc_graph_hierarchy(H, graph_type, layers={}):
+    """Assign nodes a y-coordinate, according to their distance to the principal"""
     if graph_type == "root":
         root_node_paths = [
             x for x in nx.all_pairs_shortest_path(H) if not H.in_edges(x[0])
@@ -52,13 +56,14 @@ def calc_graph_hierarchy(H, graph_type, layers={}):
         root_nodes = find_terminating_nodes(H, graph_type)
         for node in root_nodes:
             layers[node] = cur_layer
-        H = remove_from_graph(H, root_nodes)
+        H = remove_nodes_from_graph(H, root_nodes)
         if H.nodes:
             _ = calc_graph_hierarchy(H, graph_type, layers)
     return layers
 
 
 def find_terminating_nodes(H, graph_type):
+    """Find either the root of leaf nodes of a graph H"""
     term_nodes = []
     for node in H.nodes:
         edges = H.in_edges(node) if graph_type == "leaf" else H.out_edges(node)
@@ -67,33 +72,65 @@ def find_terminating_nodes(H, graph_type):
     return term_nodes
 
 
-def remove_from_graph(H, nodes):
+def remove_nodes_from_graph(H, nodes):
+    """Remove a list of nodes and any edges in or out from the graph"""
     for node in nodes:
-        child_nodes = [n[1] for n in H.out_edges(node)]
-        H.remove_edges_from([(node, c) for c in child_nodes])
+        H.remove_edges_from([(node, c) for c in [n[1] for n in H.out_edges(node)]])
+        H.remove_edges_from([(c, node) for c in [n[1] for n in H.in_edges(node)]])
         H.remove_node(node)
     return H
 
 
 def enforce_max_depth(G, node, graph_type, max_depth):
+    """Prune the graph, by removing nodes > max_depth distance from the principal"""
     H = G.reverse(copy=True) if graph_type == "leaf" else G
     cur_node_paths = [x for x in nx.all_pairs_shortest_path(H) if x[0] == node][0][1]
     for node, path in cur_node_paths.items():
         if len(path) > max_depth + 1:
-            remove_from_graph(G, [node])
+            remove_nodes_from_graph(G, [node])
     return G
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dir', nargs=1, type=str, help="root directory, containing the functions in a subdirectory called `code`")
-    parser.add_argument('-n', '--node', nargs=1, type=str, help="the leaf or root princpal node")
-    parser.add_argument('-t', '--type', nargs=1, type=str, help="relationship of the node to the graph: `leaf` or `root`")
-    parser.add_argument('-m', '--max_depth', nargs='?', default=-1, type=int, help="maximum number of edges between the principal node and other nodes")
-    args = parser.parse_args()  
-    root_dir, node, graph_type, max_depth = args.dir[0], args.node[0], args.type[0], args.max_depth
+    parser.add_argument(
+        "-d",
+        "--dir",
+        nargs=1,
+        type=str,
+        help="root directory, containing the functions in a subdirectory called `code`",
+    )
+    parser.add_argument(
+        "-n", "--node", nargs=1, type=str, help="the leaf or root princpal node"
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        nargs=1,
+        type=str,
+        help="relationship of the node to the graph: `leaf` or `root`",
+    )
+    parser.add_argument(
+        "-m",
+        "--max_depth",
+        nargs="?",
+        default=-1,
+        type=int,
+        help="maximum number of edges between the principal node and other nodes",
+    )
+    args = parser.parse_args()
+    root_dir, node, graph_type, max_depth = (
+        args.dir[0],
+        args.node[0],
+        args.type[0],
+        args.max_depth,
+    )
     depth_string = {True: "maximum graph depth", False: "depth {}".format(max_depth)}
-    print("Creating graph of function `{}` as {}, to {}".format(node, graph_type, depth_string[max_depth==-1]))
+    print(
+        "Creating graph of function `{}` as {}, to {}".format(
+            node, graph_type, depth_string[max_depth == -1]
+        )
+    )
 
     get_edges.process_dir(root_dir, force=False)
     G = nx.DiGraph()
